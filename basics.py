ac3934a27414b8e4c7ab0028fa407bc48d5fa65d
HECTOR1DIAZ <hector1diazr@gmail.com>
2019-07-02 14:48:51 -0500

    first commit

---
+++ basics.py
@@ -0,0 +1,42 @@
+# 1.basic
+# for x in range(151):
+#     print(x)
+
+# 2.multiples of 5
+# for x in range(5,1001,5):
+#     print(x)
+
+# 3.counting the dojo way
+# for x in range(1,101):
+#     if(x % 10 == 0):
+#         print("coding dojo")
+#     elif(x % 5 == 0):
+#         print("coding")
+#     else:
+#         print(x)
+
+# 5.woah that suckers huge
+# sum = 0
+# for x in range(500001):
+#     if x % 2 !=0:
+#         sum = sum + x
+#         print(x)
+# print(sum)
+
+# 6.countdown by fours
+# for x in range(2018,0,-4):
+#     print(x)
+
+
+# 7. flexible counter
+lownum=2
+highnum=10
+mult=2
+
+for x in range(2,11,1):
+    if x % 2 == 0:
+        print(x)
+
+
+
+        
