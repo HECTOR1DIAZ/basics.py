865673a69314a787693d21833d90131716f526dc
HECTOR1DIAZ <hector1diazr@gmail.com>
2019-07-06 00:33:29 -0500

    first commit

---
+++ basics2.py
@@ -0,0 +1,29 @@
+# def countdown():
+#   for x in range(10, -1 , -1):
+#         print(x)
+# countdown()
+
+
+# def print_and_return(list):
+#   print (list[0])
+#   return list[1]
+
+# def firstPlusLength()
+# sum = 0
+# sum = list[0] + list[length]
+
+# def greater(list):
+#   sub_list = []
+#   if len(list) == 0:
+#     return False
+#   for x in list:
+#     if arr[x] > arr[1]:
+#       sub_list.append(list[x])
+#   print(sub_list)
+#   print(len(sub_list))
+
+def this_that(num1, num2):
+  sub_list = []
+  for x in range(0, num1):
+    sub_list.append(num2)
+  return sub_list
